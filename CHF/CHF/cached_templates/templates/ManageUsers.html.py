cfcb64a76888b03fe8564e402388d82a11140cc9
hesto2-Desktop <hestermancarter@gmail.com>
2015-02-01 16:00:00 -0700

    Added Forms

--- CHF/CHF/cached_templates/templates/ManageUsers.html.py
+++ CHF/CHF/cached_templates/templates/ManageUsers.html.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1422827376.056867
+_modified_time = 1422829425.751866
 _enable_loop = True
 _template_filename = 'C:\\Dev\\Intex2\\CHF\\CHF\\templates/ManageUsers.html'
 _template_uri = 'ManageUsers.html'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['list', 'listTitle', 'form']
+_exports = ['form', 'list', 'listTitle']
 
 
 def _mako_get_namespace(context, name):
@@ -28,13 +28,13 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        users = context.get('users', UNDEFINED)
+        def form():
+            return render_form(context._locals(__M_locals))
         def list():
             return render_list(context._locals(__M_locals))
         def listTitle():
             return render_listTitle(context._locals(__M_locals))
-        def form():
-            return render_form(context._locals(__M_locals))
+        users = context.get('users', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n    ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'listTitle'):
@@ -56,20 +56,34 @@ def render_body(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_form(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def form():
+            return render_form(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n        <form action="#" method="POST">\r\n        <table>\r\n\r\n                <tr>\r\n\t\t\t\t\t<td> First Name <input type="text"/></td>\r\n                    <td class="pictureCell" > <a  href="#"><img height="150"src="https://marriottschool.byu.edu/msmadmin/securefile/empphoto/?file=b0%2F5352.jpg"/></a></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Last Name <input type="text"/></td><td> Address <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> City <input type="text"/></td><td> State <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Zip <input type="text"/></td><td> Email <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Security Question <input type="text"/></td><td> Security Answer <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Client Type <input type="text"/></td><td> Phone Number <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\r\n        </table>\r\n\r\n             <div class="submitBtns">\r\n                 <a class="resetLink"href="#">Reset Password</a>\r\n                <input class="btn btn-primary" value="Update" type="submit"/>\r\n                <input class="btn btn-danger" value="Archive" type="submit"style="margin: 10px;"/>\r\n            </div>\r\n        </form>\r\n    ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 def render_list(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        users = context.get('users', UNDEFINED)
         def list():
             return render_list(context)
+        users = context.get('users', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n')
         for client in users:
-            __M_writer('            <tr><td><a href="#" style="color:black">')
+            __M_writer('            <tr><td><a href="/CHF/ManageUsers/')
+            __M_writer(str(client.id))
+            __M_writer('">')
             __M_writer(str(client.user.first_name))
             __M_writer(' ')
             __M_writer(str(client.user.last_name))
-            __M_writer('</a></td></tr>\r\n')
+            __M_writer('</a> </td></tr>\r\n')
         __M_writer('    ')
         return ''
     finally:
@@ -88,20 +102,8 @@ def render_listTitle(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_form(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def form():
-            return render_form(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n        <form action="#" method="POST">\r\n        <table>\r\n\r\n                <tr>\r\n\t\t\t\t\t<td> First Name <input type="text"/></td>\r\n                    <td class="pictureCell" > <a  href="#"><img height="150"src="https://marriottschool.byu.edu/msmadmin/securefile/empphoto/?file=b0%2F5352.jpg"/></a></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Last Name <input type="text"/></td><td> Address <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> City <input type="text"/></td><td> State <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Zip <input type="text"/></td><td> Email <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Security Question <input type="text"/></td><td> Security Answer <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td> Client Type <input type="text"/></td><td> Phone Number <input type="text"/></td>\r\n\t\t\t\t</tr>\r\n\r\n        </table>\r\n\r\n             <div class="submitBtns">\r\n                 <a class="resetLink"href="#">Reset Password</a>\r\n                <input class="btn btn-primary" value="Update" type="submit"/>\r\n                <input class="btn btn-danger" value="Archive" type="submit"style="margin: 10px;"/>\r\n            </div>\r\n        </form>\r\n    ')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 """
 __M_BEGIN_METADATA
-{"line_map": {"97": 17, "66": 9, "67": 10, "68": 11, "69": 11, "70": 11, "39": 1, "72": 11, "73": 13, "71": 11, "44": 5, "103": 97, "79": 3, "27": 0, "49": 13, "91": 17, "85": 3, "59": 9}, "uri": "ManageUsers.html", "source_encoding": "ascii", "filename": "C:\\Dev\\Intex2\\CHF\\CHF\\templates/ManageUsers.html"}
+{"uri": "ManageUsers.html", "source_encoding": "ascii", "filename": "C:\\Dev\\Intex2\\CHF\\CHF\\templates/ManageUsers.html", "line_map": {"65": 17, "27": 0, "81": 11, "39": 1, "99": 3, "105": 99, "71": 9, "44": 5, "78": 9, "79": 10, "80": 11, "49": 13, "82": 11, "83": 11, "84": 11, "85": 11, "86": 11, "87": 13, "59": 17, "93": 3}}
 __M_END_METADATA
 """
--- CHF/CHF/views/ManageUsers.py
+++ CHF/CHF/views/ManageUsers.py
@@ -1,6 +1,8 @@
 __author__ = 'Carter'
 from django.http import HttpRequest
 from django_mako_plus.controller import view_function
+from django import forms
+from django.http import HttpResponseRedirect, HttpResponse
 import CHF.models as cMod
 from django_mako_plus.controller.router import get_renderer
 
@@ -12,4 +14,29 @@ def process_request(request):
 
     params['users'] = cMod.Client.objects.all()
 
-    return templater.render_to_response(request, 'ManageUsers.html',params)
+    return templater.render_to_response(request, 'ManageUsers.html', params)
+
+@view_function
+def edit(request):
+    params = {}
+    params['users'] = cMod.Client.objects.all()
+    try:
+       User = cMod.Client.objects.get(id=request.urlparams[0])
+    except cMod.Client.DoesNotExist:
+        return HttpResponseRedirect('/CHF/ManageUsers')
+
+    params['User'] = User
+    form = UserEditForm(initial={
+        'first_name': User.user.first_name,
+        'email': User.user.email,
+        'last_name': User.user.last_name,
+    })
+    params['form'] = form
+
+    return templater.render_to_response(request, 'ManageUsers.html', params)
+
+
+class UserEditForm(forms.Form):
+    first_name = forms.CharField(required=True, max_length=25)
+    last_name = forms.CharField(required=True, max_length=25)
+    email = forms.CharField(required=True, max_length=25)
